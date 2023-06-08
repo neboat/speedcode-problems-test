37ac9fac24a8904757e0bcea6df94c0374732b50
TB Schardl <neboat@mit.edu>
2023-06-08 02:20:40 +0000

    [find-zero-byte] Update find-zero-byte to use new general Makefile and tiering code.

--- find-zero-byte/driver.cpp
+++ find-zero-byte/driver.cpp
@@ -1,13 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cilk/cilk.h>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <random>
 #include <sys/time.h>
 #include <unistd.h>
-#include <cilk/cilk.h>
 #include <vector>
-#include <iostream>
-#include <fstream>
-#include <cstdint>
 
 #include <nanobench.h>
 
@@ -43,6 +44,7 @@
 //	    return nullptr;
 //}
 
+__attribute__((weak))
 const uint8_t* original_solution_tier1(
 		    uint8_t needle,
 		        const uint8_t* start, 
@@ -55,6 +57,7 @@ const uint8_t* original_solution_tier1(
 	    return nullptr;
 }
 
+__attribute__((weak))
 const uint8_t* original_solution_tier2(uint8_t needle, const uint8_t* start, const uint8_t* end) {
 	  for (; start < end; start += 4) {
 		      if(haszero(*reinterpret_cast<const uint32_t*>(start))) {
@@ -66,6 +69,7 @@ const uint8_t* original_solution_tier2(uint8_t needle, const uint8_t* start, con
 	  return nullptr;
 }
 
+__attribute__((weak))
 const uint8_t* original_solution_tier3(uint8_t needle, const uint8_t* start, const uint8_t* end) {
 	  for (; start < end; start += 8) {
 		      if(haszero_64b(*reinterpret_cast<const uint64_t*>(start))) {
@@ -78,6 +82,7 @@ const uint8_t* original_solution_tier3(uint8_t needle, const uint8_t* start, con
 	  return nullptr;
 }
 
+__attribute__((weak))
 const uint8_t* original_solution_tier4(uint8_t needle, const uint8_t* start, const uint8_t* end) {
 	__m128i needles = _mm_set1_epi8(needle);
 	for (; start < end; start += 16) {
@@ -91,7 +96,8 @@ const uint8_t* original_solution_tier4(uint8_t needle, const uint8_t* start, con
 }
 
 
-TEST_CASE("Tiers", "[correctness],[tier1]") {
+// TEST_CASE("Tiers", "[correctness],[tier1]") {
+TEST_CASE("Correctness", "[correctness]") {
   size_t N = 10000*16;
   std::vector<uint8_t> data(N);
   ankerl::nanobench::Rng rng;
@@ -104,52 +110,169 @@ TEST_CASE("Tiers", "[correctness],[tier1]") {
      //if (i == N/2) data[i] = 0;
   }
   auto reference = original_solution_tier1;
-  auto solution = original_solution_tier2;
-  auto solution2 = original_solution_tier3;
-  auto solution3 = original_solution_tier4;
+  // auto solution = original_solution_tier2;
+  // auto solution2 = original_solution_tier3;
+  // auto solution3 = original_solution_tier4;
   auto solution4 = solution_entry;
 
   for (int i = 0; i < 1024; i++) {
     size_t offset = 16*rng.bounded(N / 16);
     auto ret_ref = reference(0, (&data[0])+offset, std::min((&data[0]) + range, (&data[0]) + data.size()));
-    auto ret_sol = solution(0, (&data[0])+offset, std::min((&data[0]) + range,(&data[0]) + data.size()));
-    auto ret_sol2 = solution2(0, (&data[0])+offset, std::min((&data[0]) + range, (&data[0])+data.size()));
-    auto ret_sol3 = solution3(0, (&data[0])+offset, std::min((&data[0]) + range, (&data[0]) + data.size()));
+    auto ret_sol = solution_entry(0, (&data[0])+offset, std::min((&data[0]) + range, (&data[0]) + data.size()));
+    // auto ret_sol = solution(0, (&data[0])+offset, std::min((&data[0]) + range,(&data[0]) + data.size()));
+    // auto ret_sol2 = solution2(0, (&data[0])+offset, std::min((&data[0]) + range, (&data[0])+data.size()));
+    // auto ret_sol3 = solution3(0, (&data[0])+offset, std::min((&data[0]) + range, (&data[0]) + data.size()));
     REQUIRE(ret_ref == ret_sol);
-    REQUIRE(ret_sol == ret_sol2);
-    REQUIRE(ret_sol2 == ret_sol3);
+    // REQUIRE(ret_sol == ret_sol2);
+    // REQUIRE(ret_sol2 == ret_sol3);
   }
 
 
 
-  auto bencher = ankerl::nanobench::Bench().epochs(10).minEpochIterations(2).performanceCounters(true).relative(true).minEpochTime(std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::milliseconds(200))).run("Tier0", [&] {
-                        size_t offset = 16*rng.bounded(N/16);
-			auto ret = reference(0, (&data[0])+offset, std::min((&data[0]) + offset + range, (&data[0]) +data.size()));
+  // auto bencher = ankerl::nanobench::Bench().epochs(10).minEpochIterations(2).performanceCounters(true).relative(true).minEpochTime(std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::milliseconds(200))).run("Tier0", [&] {
+  //                       size_t offset = 16*rng.bounded(N/16);
+  // 			auto ret = reference(0, (&data[0])+offset, std::min((&data[0]) + offset + range, (&data[0]) +data.size()));
 
-		});
-  bencher.run("Tier1", [&] {
-		        size_t offset = 16*rng.bounded(N/16);
-			auto ret = solution(0, (&data[0])+offset, std::min((&data[0]) + offset + range, (&data[0]) + data.size()));
-			          });
-  bencher.run("Tier2", [&] {
-		        size_t offset = 16*rng.bounded(N/16);
-			auto ret = solution2(0, (&data[0])+offset, std::min((&data[0]) + offset + range, (&data[0]) + data.size()));
-			          });
-  bencher.run("Tier3", [&] {
-		        size_t offset = 16*rng.bounded(N/16);
-			auto ret = solution3(0, (&data[0])+offset, std::min((&data[0]) + offset + range, (&data[0]) + data.size()));
-			          });
-  bencher.run("Submission", [&] {
-		        size_t offset = 16*rng.bounded(N/16);
-			auto ret = solution4(0, (&data[0])+offset, std::min((&data[0]) + offset + range, (&data[0]) + data.size()));
-			          });
+  // 		});
+  // bencher.run("Tier1", [&] {
+  // 		        size_t offset = 16*rng.bounded(N/16);
+  // 			auto ret = solution(0, (&data[0])+offset, std::min((&data[0]) + offset + range, (&data[0]) + data.size()));
+  // 			          });
+  // bencher.run("Tier2", [&] {
+  // 		        size_t offset = 16*rng.bounded(N/16);
+  // 			auto ret = solution2(0, (&data[0])+offset, std::min((&data[0]) + offset + range, (&data[0]) + data.size()));
+  // 			          });
+  // bencher.run("Tier3", [&] {
+  // 		        size_t offset = 16*rng.bounded(N/16);
+  // 			auto ret = solution3(0, (&data[0])+offset, std::min((&data[0]) + offset + range, (&data[0]) + data.size()));
+  // 			          });
+  // bencher.run("Submission", [&] {
+  // 		        size_t offset = 16*rng.bounded(N/16);
+  // 			auto ret = solution4(0, (&data[0])+offset, std::min((&data[0]) + offset + range, (&data[0]) + data.size()));
+  // 			          });
 
-  auto results = bencher.results();
+  // auto results = bencher.results();
 
-  std::vector<double> runtimes;
-  for (int i = 0; i < results.size(); i++) {
-     runtimes.push_back(results[i].average(ankerl::nanobench::Result::Measure::elapsed));
-  }
-  REQUIRE(runtimes[0]/runtimes[1] >= 0.95);
+  // std::vector<double> runtimes;
+  // for (int i = 0; i < results.size(); i++) {
+  //    runtimes.push_back(results[i].average(ankerl::nanobench::Result::Measure::elapsed));
+  // }
+  // REQUIRE(runtimes[0]/runtimes[1] >= 0.95);
 }
 
+struct input_t {
+  const size_t N = 1 << 28;
+  std::vector<uint8_t> data;
+  size_t range = 0;
+  int iters = 0;
+
+  std::random_device dev;
+  ankerl::nanobench::Rng rng;
+
+  input_t(size_t _range = 4096, int _iters = 4096)
+      : data(N), range(_range), iters(_iters), rng(dev()) {
+    // Randomly initialize data.
+    for (size_t i = 0; i < N; ++i) {
+      data[i] = 1 + rng.bounded(255);
+    }
+  }
+  ~input_t() {}
+
+  std::string current_size_description() const {
+    return std::to_string(range);
+  }
+
+  bool grow_input() {
+    range = range * 14 / 10;
+    iters = iters * 14 / 10;
+    return range <= N;
+  }
+
+  void run() {
+    // Run multiple iterations, since each random placement of a 0 in
+    // the range will affect the running time.
+    for (int i = 0; i < iters; ++i) {
+      size_t offset = 16 * rng.bounded((N - range) / 16);
+
+      // Set a zero in the upper part of the range.
+      size_t zero_loc = range - 1 - rng.bounded(range / 2);
+      uint8_t saved = data[offset + zero_loc];
+      data[offset + zero_loc] = 0;
+
+      const uint8_t *result =
+          solution_entry(0, (&data[0]) + offset, (&data[0]) + offset + range);
+
+      // printf("%p--%p\n", (const uint8_t *)(&data[0]) + offset, (const uint8_t *)(&data[0]) + offset + range);
+      // const uint8_t *result = original_solution_tier4(
+      //     0, (&data[0]) + offset, (&data[0]) + offset + range);
+      // printf("%p--%p, %p\n", (const uint8_t *)(&data[0]) + offset, (const uint8_t *)(&data[0]) + offset + range,
+      // 	     result);
+      // REQUIRE((result - (const uint8_t *)(&data[offset])) == zero_loc);
+      data[offset + zero_loc] = saved;
+    }
+  }
+};
+
+const int TIER_TIMEOUT_MS = 100;
+const int NUM_EPOCHS = 3;
+
+// General method for measuring performance tiers.
+//
+// The template type IN_T must be a constructible object that supports
+// the following methods:
+// - grow_input(): Increase the input size for the next tier.
+// - current_size_description: Get an std::string describing this tier.
+// - run: Run solution_entry() on the current tier's input.
+template <typename IN_T> void measure_tiers() {
+  IN_T input;
+
+  ankerl::nanobench::Bench b;
+  b.epochs(NUM_EPOCHS);
+  b.warmup(1);
+
+  const double target =
+      std::chrono::duration<double>(std::chrono::milliseconds(TIER_TIMEOUT_MS))
+          .count();
+  double result_tier = 0.0;
+  int tier = 1;
+  while (true) {
+    b.run(input.current_size_description().c_str(), [&]() { input.run(); });
+
+    const std::vector<ankerl::nanobench::Result> results = b.results();
+    double result =
+        results[tier - 1].median(ankerl::nanobench::Result::Measure::elapsed);
+
+    if (result > target) {
+      result_tier = tier - 1;
+      // Add some fractional tier based on how close this running time
+      // is to the next tier.
+      double overhead = target / result;
+      result_tier += (overhead * overhead);
+      break;
+    }
+
+    ++tier;
+    if (!input.grow_input()) {
+      // printf("Maximum tier reached!\n");
+      REQUIRE("Maximum tier reached!");
+      result_tier = tier;
+      break;
+    }
+  }
+
+  // This assertion should never fail, but is included to ensure the
+  // report picks it up (using the -s flag).
+  REQUIRE(result_tier);
+
+  ankerl::nanobench::render(
+      "{{#result}} {{title}} {{name}} {{median(elapsed)}} {{median(pagefaults)}} "
+      "{{median(branchinstructions)}} {{median(branchmisses)}}\n",
+      b, std::cout);
+  std::string filename{"tier1.json"};
+  std::fstream s{filename, s.trunc | s.in | s.out};
+  ankerl::nanobench::render(ankerl::nanobench::templates::json(), b, s);
+}
+
+TEST_CASE("Tiers", "[tier1]") {
+  measure_tiers<input_t>();
+}
